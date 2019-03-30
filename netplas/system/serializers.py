187a8ebc9c751e1aa7a9a16ab72fbd59cb6a3315
Ümit Öztürk <1150606004@nku.edu.tr>
2019-03-30 21:00:27 +0300

    Created Client, Supplier, RawOrder, ProductOrder serializers

---
+++ netplas/system/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from system.models import Client, Supplier, RawOrder, ProductOrder
+
+
+class ClientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Client
+        fields = ('id', 'email', 'name', 'surname', 'phone', 'created_at', )
+
+
+class SupplierSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Supplier
+        fields = ('id', 'email', 'name', 'surname', 'phone', 'created_at', )
+
+
+class RawOrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RawOrder
+        fields = ('id', 'name', 'status', 'created_at', 'updated_at', )
+
+
+class ProductOrderSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProductOrder
+        fields = ('id', 'name', 'status', 'created_at', 'updated_at', )
