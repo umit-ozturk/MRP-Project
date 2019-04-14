ea16b5055733fa75525ccdad0f4ea7323d79d9fa
Ümit Öztürk <1150606004@nku.edu.tr>
2019-04-14 15:00:38 +0300

    Total Order Price is Calculated

--- netplas/system/models.py
+++ netplas/system/models.py
@@ -1,5 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 from system.constant import *
 from profile.models import UserProfile
 
@@ -43,6 +45,7 @@ class ProductOrder(models.Model):
                                on_delete=models.CASCADE)
     name = models.CharField(_('Ürün İsmi'), null=True, blank=True, max_length=150)
     quantitiy = models.PositiveIntegerField(_('Siparişteki Ürün Sayısı'), null=True, blank=True)
+    unit_price = models.PositiveIntegerField(_('Ürünün Birim Fiyatı'), null=True, blank=True)
     status = models.CharField(_('Ürün Siparişin Durumu'), choices=PRODUCT_ORDER_STATUS, default=WAITING,  max_length=150)
     created_at = models.DateTimeField(_('Kayıt Tarihi'), auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(_('Güncellenme Tarihi'), auto_now=True, editable=False)
@@ -61,6 +64,7 @@ class RawOrder(models.Model):
                                  on_delete=models.CASCADE)
     name = models.CharField(_('Hammadde İsmi'), null=True, blank=True, max_length=150)
     quantitiy = models.PositiveIntegerField(_('Siparişteki Hammadde Sayısı'), null=True, blank=True)
+    unit_price = models.PositiveIntegerField(_('Hammaddenin Birim Fiyatı'), null=True, blank=True)
     status = models.CharField(_('Hammadde Siparişin Durumu'), choices=RAW_ORDER_STATUS, default=WAITING, max_length=150)
     created_at = models.DateTimeField(_('Kayıt Tarihi'), auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(_('Güncellenme Tarihi'), auto_now=True, editable=False)
@@ -92,3 +96,21 @@ class Budget(models.Model):
 
     def __str__(self):
         return '{}'.format(self.user)
+
+
+@receiver(pre_save, sender=Budget)
+def set_total(sender, **kwargs):
+    instance = kwargs["instance"]
+    if kwargs["instance"].id is None:
+        total = Budget.objects.filter(user=kwargs["instance"].user).first()
+
+        if total is not None:
+            if instance.product_order:
+                instance.total = total.total + instance.product_order.unit_price * instance.product_order.quantitiy
+            elif kwargs["instance"].raw_order:
+                instance.total = total.total - instance.raw_order.unit_price * instance.raw_order.quantitiy
+        else:
+            if instance.product_order:
+                instance.total = kwargs["instance"].product_order.unit_price * instance.product_order.quantitiy
+            elif instance.raw_order:
+                instance.total = 0 - instance.raw_order.unit_price * instance.raw_order.quantitiy
