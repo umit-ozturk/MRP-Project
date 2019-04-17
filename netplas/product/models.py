447e9bec50052cd57d6aeaa11a1e5ee578ade847
Ümit Öztürk <1150606004@nku.edu.tr>
2019-04-17 13:52:01 +0300

    DamagedRaw model is Created

--- netplas/product/models.py
+++ netplas/product/models.py
@@ -47,3 +47,17 @@ class Raw(models.Model):
 
     def __str__(self):
         return '{}'.format(self.name)
+
+
+class DamagedRaw(models.Model):
+    raw = models.ForeignKey(Raw, on_delete=models.CASCADE, verbose_name=_('Ham madde'))
+    created_at = models.DateTimeField(_('Kayıt Tarihi'), auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(_('Güncellenme Tarihi'), auto_now=True, editable=False)
+
+    class Meta:
+        verbose_name = _('Hasarlı Ham madde')
+        verbose_name_plural = _('Hasarlı Ham maddeler')
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return '{}'.format(self.raw.name)
